2ba18794df89d8a62ea938c43c4983bd70ef7ede
JeanMarcoMiranda <elecord2014@gmail.com>
2022-06-14 17:17:01 -0500

    Adding testimonials section

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,6 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { API_URL } from "../../utils/constants";
 
+// -- Images
+import img from "../../assets/living-room.jpg";
+import imgUser from "../../assets/user.png"
+
+const testimonialDummyData = [
+  {
+    img: imgUser,
+    name: "Diana Silver",
+    title: "Director",
+    company: "Philadelphia visits BAYADA Home Health Care",
+    message:
+      "I enjoyed working with your team from Adam down to the gentleman that installed our furniture.It’s been nothing but a great experience! All too often we only hear of the negative, so I thought that you may appreciate some positive!",
+  },
+  {
+    img: imgUser,
+    name: "Diana Silver",
+    title: "Director",
+    company: "Philadelphia visits BAYADA Home Health Care",
+    message:
+      "I enjoyed working with your team from Adam down to the gentleman that installed our furniture.It’s been nothing but a great experience! All too often we only hear of the negative, so I thought that you may appreciate some positive!",
+  },
+  {
+    img: imgUser,
+    name: "Diana Silver",
+    title: "Director",
+    company: "Philadelphia visits BAYADA Home Health Care",
+    message:
+      "I enjoyed working with your team from Adam down to the gentleman that installed our furniture.It’s been nothing but a great experience! All too often we only hear of the negative, so I thought that you may appreciate some positive!",
+  },
+];
+
 const HomeView = () => {
   const [productsList, setProductList] = useState([]);
 
@@ -28,26 +59,74 @@ const HomeView = () => {
   return (
     <div className="text-2xl font-semibold w-full min-h-screen">
       {/* Home liquidation */}
-      <div className="grid grid-cols-10 min-h-screen">
-        <div className="col-span-3 bg-red-500"></div>
-        <div className="col-span-7 flex flex-wrap justify-around items-center">
-          {productsList.map((product) => (
-            <CardComponent productData={product}/>
-          ))}
+      <section>
+        <h2 className="text-3xl text-center text-gray-600 my-8">
+          Current Liquidations
+        </h2>
+
+        <div className="grid grid-cols-11 min-h-screen">
+          <div className="col-span-4 bg-red-500">
+            <div className="px-8 py-12">
+              <h3 className="text-6xl text-gray-200 mb-8">90% OFF</h3>
+              <p className="text-xl text-gray-200">Check out our current liquidations taking place throughout the country for up to 90% savings on available products. Hurry! Once these items sell out they are gone for good.</p>
+            </div>
+          </div>
+          <div className="col-span-7 flex flex-wrap justify-around">
+            {productsList.map((product) => (
+              <CardComponent productData={product} />
+            ))}
+          </div>
         </div>
-      </div>
+      </section>
+
+      {/* Testimonials section */}
+      <section className="relative">
+        <img src={img} alt="tetimonials" className=" w-full object-cover" />
+
+        <div className="absolute inset-0 w-full h-full">
+          <h2 className="text-3xl text-gray-200 text-center my-8">
+            Testimonials
+          </h2>
+
+          <div className="flex w-3/4 h-96 mx-auto">
+            {testimonialDummyData.map((testimonial, index) => (
+              <TestimonialComponent testimonialData={testimonial} />
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
 
+const TestimonialComponent = ({
+  testimonialData: { img, name, title, company, message },
+}) => (
+  <div className="mx-4">
+    <figure className="w-full h-40 flex justify-center items-center">
+      <img className="w-1/3 h-3/4" src={img}/>
+    </figure>
+    <h4 className="text-center text-gray-200 text-xl mb-2">{name}</h4>
+    <p className="text-center text-gray-400 text-sm mx-8">
+      {title}, {company}
+    </p>
+    <hr className="my-6 mx-auto w-20"/>
+    <p className="text-center text-gray-200 text-sm mx-8">{message}</p>
+  </div>
+);
+
 const CardComponent = ({ productData: { img, title, desc, type, price } }) => (
-  <div className="w-72 rounded overflow-hidden shadow-lg my-6">
+  <div className="w-72 rounded overflow-hidden shadow-lg mb-6">
     <figure className="w-full h-48">
-      <img className="w-full h-full" src={img[0]} alt={`${title}`}/>
+      <img className="w-full h-full" src={img[0]} alt={`${title}`} />
     </figure>
     <div className="flex flex-col px-6 py-4">
-      <div className="text-gray-600 font-bold text-base mb-2 truncate">{title}</div>
-      <p className="h-16 text-gray-500 text-sm mb-2 scrollbar-default overflow-auto">{desc}</p>
+      <div className="text-gray-600 font-bold text-base mb-2 truncate">
+        {title}
+      </div>
+      <p className="h-16 text-gray-500 text-sm mb-2 scrollbar-default overflow-auto">
+        {desc}
+      </p>
       <p className="text-base font-bold">Price: ${price}</p>
     </div>
   </div>
